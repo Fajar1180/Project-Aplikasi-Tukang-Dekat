b66b950a9bcb86702070b3c8707be280d9d162f6
Muhammad Fajar Nurjaman <129603364+Fajar1180@users.noreply.github.com>
2026-07-29 13:02:31 +0700

    Update AppServiceProvider.php

--- backend/app/Providers/AppServiceProvider.php
+++ backend/app/Providers/AppServiceProvider.php
@@ -30,7 +30,7 @@ class AppServiceProvider extends ServiceProvider
     }
 
     /**
-     * Bootstrap any application services.
+     * Bootstrap any application services .
      */
     public function boot(): void
     {
